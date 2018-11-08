39afee843ec4db32e0e9407d4a2b002a88497928
David <dbermejosimon@gmail.com>
2018-11-08 17:38:32 +0100

    añadidos componentes a la interfaz de DialogSelectorColor

--- src/DialogSelectorColor.java
+++ src/DialogSelectorColor.java
@@ -12,13 +12,26 @@ public class DialogSelectorColor extends JDialog {
     int red;
     int green;
     int blue;
-    
+
+    JSpinner redSpinner;
+    JSpinner blueSpinner;
+    JSpinner greenSpinner;
+
+    JLabel labelRed;
+    JLabel labelBlue;
+    JLabel labelGreen;
+
+    JSlider redSlider;
+    JSlider blueSlider;
+    JSlider greenSlider;
+
+    JPanel colorMuestra;
+
     JButton okButton;
-    JButton cancelButton;
-    JButton resetButton;
 
-    JFrame frame;
 
+
+    MyButton bAffected;
     /**
      * Constructor de la clase para inicializar parámetros básicos
      * @param button : botón que ha activado el dialogo
@@ -28,31 +41,158 @@ public class DialogSelectorColor extends JDialog {
      */
     public DialogSelectorColor(JButton button,JFrame owner, String title, boolean modal) {
         super(owner, title, modal);
-        this.frame = owner;
+        bAffected = (MyButton)button;
 
         //colocamos la ventana del dialogo sobre el botón que la activo. Ademas le establecemos las medidas
-        super.setBounds((int)button.getLocationOnScreen().getX(),(int)button.getLocationOnScreen().getY(),250,400);
+        super.setBounds((int)button.getLocationOnScreen().getX()-10,(int)button.getLocationOnScreen().getY(),350,450);
         super.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-    }
 
-    /**
-     * Metodo encargado de inicializar los componentes y los listener del JDialog
-     *
-     */
-    public void startDialog(){
+        //inicializar los componentes y los listeners, además de hacer la ventana visible
+        addComponents();
+        addListener();
         this.setVisible(true);
-        inicializarComponentes();
-        inicializarListeners();
-
     }
 
-    public void inicializarComponentes(){
 
 
+    public void addComponents(){
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints settings = new GridBagConstraints();
 
+        //config valor red
+        labelRed = new JLabel("Cantidad de Rojo");
+        settings = new GridBagConstraints();
+        settings.gridy=0;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.gridwidth=2;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(labelRed,settings);
+
+
+        redSlider = new JSlider();
+        redSlider.setMajorTickSpacing(10);
+        redSlider.setMinorTickSpacing(0);
+        redSlider.setMaximum(255);
+        redSlider.setMinimum(0);
+        redSlider.setPaintTicks(true);
+        redSlider.setPaintLabels(false);
+        settings = new GridBagConstraints();
+        settings.gridy=1;
+        settings.gridx=0;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.gridwidth=2;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(redSlider,settings);
+
+
+        redSpinner = new JSpinner();
+        settings = new GridBagConstraints();
+        settings.gridy=2;
+        settings.gridx=0;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.gridwidth=2;
+
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(redSpinner,settings);
+
+
+        // config valor green
+        labelGreen = new JLabel("Cantidad de Verde");
+        settings = new GridBagConstraints();
+        settings.gridy=3;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(labelGreen,settings);
+
+
+        greenSlider = new JSlider();
+        greenSlider.setMajorTickSpacing(10);
+        greenSlider.setMinorTickSpacing(0);
+        greenSlider.setMaximum(255);
+        greenSlider.setMinimum(0);
+        greenSlider.setPaintTicks(true);
+        greenSlider.setPaintLabels(false);
+        settings = new GridBagConstraints();
+        settings.gridy=4;
+
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(greenSlider,settings);
+
+
+        greenSpinner = new JSpinner();
+        settings = new GridBagConstraints();
+        settings = new GridBagConstraints();
+        settings.gridy=5;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(greenSpinner,settings);
+
+
+        // config valor blue
+        labelBlue = new JLabel("Cantidad de Azul");
+        settings = new GridBagConstraints();
+        settings = new GridBagConstraints();
+        settings.gridy=6;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(labelBlue,settings);
+
+
+        blueSlider = new JSlider();
+        blueSlider.setMajorTickSpacing(10);
+        blueSlider.setMinorTickSpacing(0);
+        blueSlider.setMaximum(255);
+        blueSlider.setMinimum(0);
+        blueSlider.setPaintTicks(true);
+        blueSlider.setPaintLabels(false);
+        settings = new GridBagConstraints();
+        settings.gridy=7;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(blueSlider,settings);
+
+
+        blueSpinner = new JSpinner();
+        settings = new GridBagConstraints();
+        settings.gridy=8;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(blueSpinner,settings);
+
+
+        //panel que muestra el color.
+        colorMuestra = new JPanel();
+        colorMuestra.setBorder(BorderFactory.createLineBorder(Color.GRAY,3));
+        settings = new GridBagConstraints();
+        settings.gridy=9;
+        settings.gridwidth=2;
+        settings.ipadx=200;
+        settings.ipady=25;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        settings.insets = new Insets(10,0,10,0);
+        this.add(colorMuestra,settings);
+
+
+        //boton aceptar
+        okButton = new JButton("Ok!");
+        settings.gridy=10;
+        settings.ipadx=10;
+        settings.ipady=10;
+        settings.fill = GridBagConstraints.HORIZONTAL;
+        this.add(okButton,settings);
     }
 
-    public void inicializarListeners(){
+    public void addListener(){
 
     }
 
